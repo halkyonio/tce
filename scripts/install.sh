bc730ebf7c819961759c5ac82fdb9920dbdf2457
Charles Moulliard <cmoulliard@redhat.com>
2022-04-05 10:39:41 +0200

    Remove for word as not needed

--- scripts/install.sh
+++ scripts/install.sh
@@ -93,7 +93,7 @@ export KUBECONFIG=$HOME/.kube/${KUBE_CFG}
 
 SECONDS=0
 
-log "CYAN" "Install the tanzu client for version: $TCE_VERSION"
+log "CYAN" "Install the tanzu client version: $TCE_VERSION"
 curl -H "Accept: application/vnd.github.v3.raw" \
     -L https://api.github.com/repos/vmware-tanzu/community-edition/contents/hack/get-tce-release.sh | \
     bash -s $TCE_VERSION linux
