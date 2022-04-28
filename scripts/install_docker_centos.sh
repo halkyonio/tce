27ec5014584a259ef6f6c5b892a3fcc96d7e9d3f
Charles Moulliard <cmoulliard@redhat.com>
2022-04-28 14:32:01 +0200

    Refactor the code to support to install using different distro

--- scripts/install_docker_centos.sh
+++ scripts/install_docker_centos.sh
@@ -4,7 +4,7 @@
 #
 # Execute this command locally
 #
-# ./install_docker_fed.sh
+# ./install_docker_centos.sh
 #
 # Define the following env vars:
 # - REMOTE_HOME_DIR: home directory where files will be installed within the remote VM
@@ -46,11 +46,27 @@ log() {
   echo; repeat_char ${1} '#'; log_msg ${1} ${MSG}; repeat_char ${1} '#'; echo
 }
 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo  https://download.docker.com/linux/fedora/docker-ce.repo
+log "CYAN" "Check the linux distro, calculate the new path to get the docker dnf repo"
+LINUX_DISTRO_NAME=$(awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release)
+if [ "$LINUX_DISTRO_NAME" = "Rocky Linux" ]; then
+    REPO_SUBPATH="centos"
+elif [ "$LINUX_DISTRO_NAME" = "Fedora Linux" ]; then
+    REPO_SUBPATH="fedora"
+    sudo dnf -y install dnf-plugins-core
+else
+   REPO_SUBPATH="centos"
+fi
+
+log "CYAN" "Add the docker dnf repo"
+sudo dnf config-manager --add-repo https://download.docker.com/linux/$REPO_SUBPATH/docker-ce.repo
 #sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo dnf install docker-ce docker-ce-cli containerd.io
+log "CYAN" "Install docker-ce docker-ce-cli containerd.io"
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
+
+log "CYAN" "Launch the docker daemon"
 sudo systemctl start docker
 sudo systemctl status docker
-sudo groupadd docker
+
+log "CYAN" "Add groupadd docker and usermod -aG docker $USER"
+sudo groupadd -f docker
 sudo usermod -aG docker $USER
