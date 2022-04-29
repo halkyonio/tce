37875006197e741d6603d3821ede6c997dd3b1a0
Charles Moulliard <cmoulliard@redhat.com>
2022-04-29 07:32:06 +0200

    Refactor commands to install using yum or dnf. TODO

--- scripts/install_docker_centos.sh
+++ scripts/install_docker_centos.sh
@@ -49,19 +49,22 @@ log() {
 log "CYAN" "Check the linux distro, calculate the new path to get the docker dnf repo"
 LINUX_DISTRO_NAME=$(awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release)
 if [ "$LINUX_DISTRO_NAME" = "Rocky Linux" ]; then
-    REPO_SUBPATH="centos"
+    log "CYAN" "Add the docker dnf repo"
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io
 elif [ "$LINUX_DISTRO_NAME" = "Fedora Linux" ]; then
     REPO_SUBPATH="fedora"
     sudo dnf -y install dnf-plugins-core
+    log "CYAN" "Add the docker dnf repo"
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io
 else
-   REPO_SUBPATH="centos"
+    REPO_SUBPATH="centos"
+    log "CYAN" "Add docker-ce repo"
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    log "CYAN" "Install docker-ce docker-ce-cli containerd.io"
+    sudo yum -y install docker-ce docker-ce-cli containerd.io
 fi
-
-log "CYAN" "Add the docker dnf repo"
-sudo dnf config-manager --add-repo https://download.docker.com/linux/$REPO_SUBPATH/docker-ce.repo
-#sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-log "CYAN" "Install docker-ce docker-ce-cli containerd.io"
-sudo dnf -y install docker-ce docker-ce-cli containerd.io
 
 log "CYAN" "Launch the docker daemon"
 sudo systemctl start docker
