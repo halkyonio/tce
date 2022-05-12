d2944f8c00b4a8fcddb8b90faf908a4e6014053f
Charles Moulliard <cmoulliard@redhat.com>
2022-05-12 10:53:08 +0200

    Add TODO to investigate how to use imgpkg command to get the TKG version

--- scripts/list_tkr_version.sh
+++ scripts/list_tkr_version.sh
@@ -13,6 +13,9 @@
 # - REMOTE_HOME_DIR: home directory where files will be installed within the remote VM
 # - TCE_VERSION: Version of TCE used to check the TKG version compatibles
 
+# TODO: Check how we could improve this script using this command: imgpkg tag list -i projects.registry.vmware.com/tce/tkr
+# Doc: https://tanzucommunityedition.io/docs/v0.12/ref-unmanaged-cluster/#customize-the-distribution
+
 REMOTE_HOME_DIR=${REMOTE_HOME_DIR:-$HOME}
 TCE_VERSION=${TCE_VERSION:-v0.12.0}
 DIR=`dirname $0`
